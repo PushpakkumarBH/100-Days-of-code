5c0c8d575a650e4156a7257d39a016cb1e6048b1
Pushpakkumar <57779932+PushpakkumarBH@users.noreply.github.com>
2022-07-24 00:35:44 +0530

    Add files via upload
    Done with day one it took lot of time figuring out how to upload files to GitHub .
    learnt about npm packages and nothing must. The start of coding was bit stretchy but further down the road there is hope for improvements.

---
+++ day-1/index.js
@@ -0,0 +1,6 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+var mysuperheroes = superheroes.random();
+var mysupervillains = supervillains.random();
+console.log(mysuperheroes);
+console.log(mysupervillains);
