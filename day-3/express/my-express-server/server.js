8cca2aa01467bde1ac5c84d13c0d8ed8ab4274a7
Pushpakkumar <57779932+PushpakkumarBH@users.noreply.github.com>
2022-07-31 01:38:01 +0530

    First commit

--- day-3/express/my-express-server/server.js
+++ day-3/express/my-express-server/server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!<h1/>')
+app.get('/', function(req, res) {
+  res.send('<h1>Hello World...<h1/>')
 })
 
-app.listen(port, () => {
+app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/contact',function(req,res){
+  res.send('<h1>contact me @ pushpak696@gmail.com</h1>')
+})
+
+app.get('/about',function(req,res){
+  res.send('<h3>Hi There myself pushpakkumar studying in tumkur i am doing my b.tech in ece</h3>')
 })
